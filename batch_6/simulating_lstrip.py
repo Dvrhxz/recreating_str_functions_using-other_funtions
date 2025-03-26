713d859e151a5e5170720f0e4c4242065cfbd528
my name <you@example.com>
2025-03-26 16:59:25 +0800

    prog 1 recreated .lstrip() by making program print text at a point after leading spaces

--- batch_6/simulating_lstrip.py
+++ batch_6/simulating_lstrip.py
@@ -1,3 +1,12 @@
 # enter word/statement with spaces
-# if input has spaces at the beginning, remove spaces
-# print the output
+word = input("Enter a word: ")
+
+# loops through each character
+# if it starts with space increment by one until there is a valid character in that index
+character = 0
+
+while character < len(word) and word[character] == " ":
+    character += 1
+
+# if the while loop reaches index with valid character, print starting from that point
+print(f"Output: {word[character:]}")
