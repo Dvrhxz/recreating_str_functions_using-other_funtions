d4c8a3c349f3e0a0a6cacd66ace30e64422e1c34
my name <you@example.com>
2025-03-28 21:52:00 +0800

    prog 3 recreated .lower() using .swapcase instead of .lower()

--- batch_6/simulating_lower.py
+++ batch_6/simulating_lower.py
@@ -1,4 +1,14 @@
 # input word/statement
+word = input("Enter a statement: ")
+lowered_word = ""
+
 # check if word is upper
-# if true, swap case
+for character in word:
+    if "A" <= character <= "Z":
+        # if true, swap case
+        lowered_word += character.swapcase()
+    else:
+        lowered_word += character
+
 # print result
+print(f"Output: {lowered_word}")
