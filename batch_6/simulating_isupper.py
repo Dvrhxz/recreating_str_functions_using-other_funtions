fa328e3a5eb85179cdb5a1386de988787082007f
my name <you@example.com>
2025-03-28 22:06:35 +0800

    prog 4 recreated .isupper() using .islower() instead

--- batch_6/simulating_isupper.py
+++ batch_6/simulating_isupper.py
@@ -1,3 +1,14 @@
 # Input word/statement
+word = input("Enter a statement: ")
+all_upper = True
+
 # Check if word is upper or not
+for character in word:
+    if character.islower():
+        all_upper = False
+
 # Print result if all isupper or not
+if all_upper:
+    print("All characters are uppercase.")
+else:
+    print("Not all characters are uppercase.")
