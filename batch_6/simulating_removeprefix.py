738603926b2f5ff7698e145dbd9c3966c26237df
my name <you@example.com>
2025-03-28 21:39:05 +0800

    prog 2 recreated .removeprefix() using .startswith()

--- batch_6/simulating_removeprefix.py
+++ batch_6/simulating_removeprefix.py
@@ -1,4 +1,12 @@
 # input a statement
+statement = input("Enter a statement: ")
+
 # input a prefix
+prefix =  input("enter a prefix to remove: ")
+
 # remove prefix from statement without .removeprefix()
+if statement.startswith(prefix):
+    statement = statement[len(prefix):]
+
 # print output
+print(f"Results: {statement}")
