96773d6f3d3fdef7ea38c119c73ec79842cb944c
my name <you@example.com>
2025-03-28 22:20:07 +0800

    prog 5 recreated .endswith() by basing it off of indexes

--- batch_6/simulating_endswith.py
+++ batch_6/simulating_endswith.py
@@ -1,4 +1,13 @@
 # enter word/statement
+word = input("Enter a statement: ")
+
 # enter ending word/statement
+ending = input("Enter the ending word: ")
+
 # check if both are the same
-# print result
+if word[len(word) - len(ending):] == ending:
+    # print result
+    print("The ending of the inputted word matches with the inputted ending.")
+else:
+    print("The word does not match with the inputted ending.")
+
