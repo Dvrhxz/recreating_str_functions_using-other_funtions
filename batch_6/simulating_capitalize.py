eac132bc6c6690e512f4e0f620294b129f4e3340
my name <you@example.com>
2025-03-28 23:08:22 +0800

    prog 9 recreated capitalize by using upper on index 0 and lower on the proceeding indexes

--- batch_6/simulating_capitalize.py
+++ batch_6/simulating_capitalize.py
@@ -1,3 +1,11 @@
 # enter word/statement
+word = input("Enter a statement: ")
+
 # make the first letter capital and the rest lowercase
+if len(word) > 0:
+    capitalized = word[0].upper() + word[1:].lower()
+else:
+    capitalized = word
+
 # print result
+print(f"Output: {capitalized}")
