f3c53430815774b63b73f55582486cf1f8a07a8d
my name <you@example.com>
2025-03-28 22:42:35 +0800

    prog 6 recreated ljust() using the len of the word and inputted length to add the remaining spaces

--- batch_6/simulating_ljust.py
+++ batch_6/simulating_ljust.py
@@ -1,4 +1,12 @@
 # enter word/statement
+word = input("Enter a word: ")
+
 # enter the length
+length = int(input("Enter the length: "))
+
 # add space characters at the end
+if len(word) < length:
+    word = word + " " * (length - len(word))
+
 # print result
+print(f"Output: '{word}'")
