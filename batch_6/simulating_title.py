479719c9c908deaed3b478fed73987a028a9947d
my name <you@example.com>
2025-03-28 23:19:46 +0800

    prog 10 recreated title using upper and lower on words that was split

--- batch_6/simulating_title.py
+++ batch_6/simulating_title.py
@@ -1,4 +1,16 @@
 # enter word/statement
+statement = input("Enter a statement: ")
+
 # make the first letter of each word capital and the rest lowercase
+words = statement.split()
+titled_word = []
+for word in words:
+    if len(word) > 0:
+        formatted_word = word[0].upper() + word[1:].lower()
+        titled_word.append(formatted_word)
+
 # join the words into a single string with spaces
+titled_word = ' '.join(titled_word)
+
 # print result
+print(f"Output: {titled_word}")
