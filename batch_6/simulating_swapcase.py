748ba15be3680b8b04d10d2e6acc152738d56940
my name <you@example.com>
2025-03-28 23:01:58 +0800

    prog 8 recreated swapcase() with a combination of islower, isupper, lower, and upper

--- batch_6/simulating_swapcase.py
+++ batch_6/simulating_swapcase.py
@@ -1,4 +1,16 @@
 # enter word/statement
+word = input("Enter a statement: ")
+
 # reverse the casing of each character
-# using isupper, islower, lower, and upper
+result = ""
+for char in word:
+    # using isupper, islower, lower, and upper
+    if char.isupper():
+        result += char.lower()
+    elif char.islower():
+        result += char.upper()
+    else:
+        result += char
+
 # print result
+print(f"Output: {result}")
