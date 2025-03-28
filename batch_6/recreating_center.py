1ded1060ee32f508fd4b648cdfb945cb85288ce2
my name <you@example.com>
2025-03-28 22:53:01 +0800

    prog 7 recreated center by basing it off of remaining length missing

--- batch_6/recreating_center.py
+++ batch_6/recreating_center.py
@@ -1,4 +1,15 @@
 # enter word/statement
+word = input("Enter a statement: ")
+
 # enter the length
+length = int(input("Enter the length: "))
+
 # add space characters at the beginning and end
+if len(word) < length:
+    total_spaces = length - len(word)
+    left_spaces = total_spaces // 2
+    right_spaces = total_spaces - left_spaces
+    word = ' ' * left_spaces + word + ' ' * right_spaces
+
 # print result
+print(f"Output: '{word}'")
