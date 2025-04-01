e5a568d1ae14953ff1ca3b659d15d65e649e1477
my name <you@example.com>
2025-04-01 16:31:48 +0800

    batch 7, prog 6 recreated rjust

--- batch_7/simulating_rjust.py
+++ batch_7/simulating_rjust.py
@@ -1,4 +1,12 @@
 # enter word/statement
+word = input("Enter a word: ")
+
 # enter the length
+length = int(input("Enter the length: "))
+
 # add space characters at the start
+if len(word) < length:
+    word = " " * (length - len(word)) + word
+
 # print result
+print(f"Output: '{word}'")
