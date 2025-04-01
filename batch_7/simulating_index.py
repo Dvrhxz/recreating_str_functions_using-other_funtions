bbf09152ac85ed22c5a2181957f13311f8b77b3b
my name <you@example.com>
2025-04-01 17:41:15 +0800

    batch 7, prog 9 recreated index using find instead since they are pretty much the same in function

--- batch_7/simulating_index.py
+++ batch_7/simulating_index.py
@@ -1,4 +1,14 @@
 # enter word/statement
+word = input("Enter a word: ")
+
 # enter the text to find
+substring = input("Enter the word/statement to find: ")
+
 # find the first occurrence
+position = word.find(substring)
+
 # print result
+if position != -1:
+    print(f'The substring "{substring}" first appears at index {position}.')
+else:
+    print("Raising ValueError")
