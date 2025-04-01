c1e459854eeaed8c0161456f92bd4c79d35af298
my name <you@example.com>
2025-04-01 16:15:08 +0800

    batch 7, prog 4 recreated islower

--- batch_7/simulating_islower.py
+++ batch_7/simulating_islower.py
@@ -1,3 +1,15 @@
 # input a statement/word
+word = input("Enter a statement: ")
+all_lower = True
+
 # check if all is lower
+for character in word:
+    if character.isupper():
+        all_lower = False
+
+
 # print result
+if all_lower:
+    print("All characters are lowercase.")
+else:
+    print("Not all characters are lowercase.")
