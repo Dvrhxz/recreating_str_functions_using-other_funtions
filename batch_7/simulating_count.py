216926b3b5d45182686221739187b9914193856d
my name <you@example.com>
2025-04-01 17:16:13 +0800

    batch 7, prog 8 recreated count

--- batch_7/simulating_count.py
+++ batch_7/simulating_count.py
@@ -1,4 +1,22 @@
 # enter word/statement
+word = input("Enter a word: ")
+
 # enter the text to count
+substring = input("Enter the word/statement to count: ")
+
 # count each time it is iterated
+count = 0
+start = 0
+
+while True:
+    start = word.find(substring, start)
+
+    if start == -1:
+        break
+
+    else:
+        count += 1
+        start += len(substring)
+
 # print result
+print(f'The substring "{substring}" appears {count} times in the text.')
