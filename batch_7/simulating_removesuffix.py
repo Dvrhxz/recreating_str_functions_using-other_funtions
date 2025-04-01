c77b6cc3a98bd04ec40617a7903f9c347480c3d3
my name <you@example.com>
2025-04-01 15:12:54 +0800

    batch 7, prog 2 recreated removesuffix

--- batch_7/simulating_removesuffix.py
+++ batch_7/simulating_removesuffix.py
@@ -1,4 +1,12 @@
 # input word/statement
+statement = input("Enter a statement: ")
+
 # input suffix
+suffix =  input("enter a suffix to remove: ")
+
 # remove the inputted suffix from the word/statement
+if statement.endswith(suffix):
+    statement = statement[:len(statement) - len(suffix)]
+
 # print results
+print(f"Output: {statement}")
