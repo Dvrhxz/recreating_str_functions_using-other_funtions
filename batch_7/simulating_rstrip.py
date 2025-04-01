1cd6690e771ba9150ccdf598cd9c681a6619b360
my name <you@example.com>
2025-04-01 14:52:02 +0800

    batch 7, prog 1 recreated rstrip

--- batch_7/simulating_rstrip.py
+++ batch_7/simulating_rstrip.py
@@ -1,3 +1,9 @@
 # input a word/statement
+word = input("Enter a word: ")
+
 # if there are trailing spaces remove them
+while len(word) > 0 and word[-1] == " ":
+    word = word[:-1]
+
 # print result
+print(f"Output: {word}")
