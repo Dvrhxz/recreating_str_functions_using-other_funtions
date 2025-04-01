602ce0293ff33ca6620d4fced4c5d3383fd5196a
my name <you@example.com>
2025-04-01 17:46:25 +0800

    batch 7, prog 10 recreated rindex using the same process as prog 9, but using rfind this time

--- batch_7/simulating_rindex.py
+++ batch_7/simulating_rindex.py
@@ -1,4 +1,14 @@
 # enter word/statement
+word = input("Enter a word: ")
+
 # enter the text to find
+substring = input("Enter the word/statement to find: ")
+
 # find the last occurrence
+position = word.rfind(substring)
+
 # print result
+if position != -1:
+    print(f'The substring "{substring}" last appears at index {position}.')
+else:
+    print("Raising ValueError")
