87198b47a5f856b849684d7c771506d3d65bf2ec
my name <you@example.com>
2025-04-01 16:47:05 +0800

    batch 7, prog 7 recreated zfill using same code as the rjust

--- batch_7/simulating_zfill.py
+++ batch_7/simulating_zfill.py
@@ -1,4 +1,12 @@
 # enter word/statement/number
+word = input("Enter a word: ")
+
 # enter the length
+length = int(input("Enter the length: "))
+
 # add 0 at the start
+if len(word) < length:
+    word = "0" * (length - len(word)) + word
+
 # print result
+print(f"Output: '{word}'")
