75c5c6147f09cfc9b6cfdf11503364eaaa2c1295
my name <you@example.com>
2025-04-01 15:28:25 +0800

    batch 7, prog 3 recreated upper

--- batch_7/simulating_upper.py
+++ batch_7/simulating_upper.py
@@ -1,4 +1,14 @@
 # input word/statement
+word = input("Enter a statement: ")
+capitalized_word = ""
+
 # check if input is lower
-# if lower swapcase to upper
+for character in word:
+    # if lower swapcase to upper
+    if character.islower():
+        capitalized_word += character.swapcase()
+    else:
+        capitalized_word += character
+
 # print results
+print(f"Output: {capitalized_word}")
