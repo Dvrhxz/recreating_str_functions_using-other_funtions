a39c5ee78d116de81aeaacf7ad41b72f8b1dd30f
my name <you@example.com>
2025-04-01 16:23:54 +0800

    batch 7, prog 5 recreated startswith

--- batch_7/simulating_startswith.py
+++ batch_7/simulating_startswith.py
@@ -1,4 +1,12 @@
 # enter word/statement
+word = input("Enter a statement: ")
+
 # enter starting word/statement
+start = input("Enter the starting word: ")
+
 # check if both are the same
-# print result
+if word[:len(start)] == start:
+    # print result
+    print("The start of the inputted word matches with the inputted starting word.")
+else:
+    print("The word does not match with the inputted start.")
